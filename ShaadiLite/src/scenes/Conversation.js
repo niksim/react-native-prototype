f518fc3c9d533e7da111da0926dc7bb504a370ea
Rakesh Sharma <rakesh.sharma@peopleinteractive.in>
2018-03-08 17:12:20 +0530

    Chat Header and Toggle Created

--- ShaadiLite/src/scenes/Conversation.js
+++ ShaadiLite/src/scenes/Conversation.js
@@ -1,38 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { Icon } from "native-base";
-
+import ChatHeader from "../components/Chat/ChatHeaders";
+import RecentActiveToggle from "../components/Chat/RecentActiveToggle";
 
 export default class Conversation extends React.Component {
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => (
+      <Icon name="ios-heart" style={{ color: tintColor }} />
+    )
+  };
 
-    static navigationOptions = {
-
-        tabBarIcon: ({ tintColor }) => (
-            <Icon name="ios-heart" style={{ color: tintColor }} />
-        )
-
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.text}>Conversation</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ChatHeader />
+        <RecentActiveToggle />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#cd486b',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        color: "white",
-        fontSize: 50,
-        opacity: 0.6,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#cd486b"
+  }
 });
-
