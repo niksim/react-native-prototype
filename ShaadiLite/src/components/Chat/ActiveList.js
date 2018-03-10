f15722270d5ecd7276f1b13567d219d6ab7adc2c
Rakesh Sharma <sharma.rakesh23@ymail.com>
2018-03-11 02:28:21 +0530

    Active and Recent List Added Using Flat List Dummy Api Call Added

---
+++ ShaadiLite/src/components/Chat/ActiveList.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react"
+import { View, Text, FlatList, ActivityIndicator } from "react-native"
+import { List, ListItem, SearchBar } from "react-native-elements"
+
+export default class ActiveList extends React.Component {
+  render() {
+    return (
+      <FlatList
+        data={this.props.data}
+        renderItem={({ item }) => (
+          <ListItem
+            roundAvatar
+            title={`${item.name.first} ${item.name.last}`}
+            subtitle={item.email}
+            avatar={{ uri: item.picture.thumbnail }}
+            containerStyle={{ borderBottomWidth: 0 }}
+          />
+        )}
+        keyExtractor={item => item.email}
+      />
+    )
+  }
+}
--- ShaadiLite/src/components/Chat/RecentActiveToggle.js
+++ ShaadiLite/src/components/Chat/RecentActiveToggle.js
@@ -1,74 +1,145 @@
-import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { Button } from "native-base";
+import React, { Component } from "react"
+import { StyleSheet, View, Text } from "react-native"
+import { Button } from "native-base"
+import RecentList from "./RecentList"
+import ActiveList from "./ActiveList"
+import axios from "axios"
 
 export default class RecentActiveToggle extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
+      data: [],
+      loading: false,
+      error: null,
       tabSelected: 0
-    };
+    }
+  }
+
+  componentDidMount() {
+    this.fetchRecentList()
+  }
+
+  async fetchRecentList() {
+    const url = `https://randomuser.me/api/?page=1&results=20`
+    this.setState({ loading: true })
+
+    const users = await axios
+      .get(url)
+      .then(res => {
+        this.setState({
+          data: res.data.results,
+          error: res.error || null,
+          loading: false
+        })
+      })
+      .catch(error => {
+        this.setState({ error, loading: false })
+      })
+  }
+  toggle = () => {
+    this.fetchRecentActiveList()
+    this.setState({ tabSelected: this.state.tabSelected == 0 ? 1 : 0 })
+  }
+
+  async fetchActiveList() {
+    const url = `https://randomuser.me/api/?page=2&results=20`
+    this.setState({ loading: true })
+
+    const users = await axios
+      .get(url)
+      .then(res => {
+        this.setState({
+          data: res.data.results,
+          error: res.error || null,
+          loading: false
+        })
+      })
+      .catch(error => {
+        this.setState({ error, loading: false })
+      })
+  }
+  toggle = () => {
+    this.setState({
+      data: [],
+      tabSelected: this.state.tabSelected == 0 ? 1 : 0
+    })
+    if (this.state.tabSelected == 0) {
+      this.fetchRecentList()
+    } else {
+      this.fetchActiveList()
+    }
+  }
+
+  getSelectedList = () => {
+    if (this.state.tabSelected == 0) {
+      return <ActiveList style={{ paddingTop: 2 }} data={this.state.data} />
+    } else {
+      return <RecentList style={{ paddingTop: 2 }} data={this.state.data} />
+    }
   }
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.buttonView}>
-          <Button full transparent onPress={() => this.toggle()}>
-            <Text
-              style={{
-                paddingTop: 3,
-                fontWeight: "bold",
-                color: this.state.tabSelected == 0 ? "#00bcd5" : "#72727d"
-              }}
-            >
-              Recent Chat's
-            </Text>
-          </Button>
-          <View
-            style={[
-              {
-                height: 3,
-                backgroundColor:
-                  this.state.tabSelected == 0 ? "#00bcd5" : "white"
-              },
-              styles.shadow
-            ]}
-          />
-        </View>
+      <View style={{ backgroundColor: "white" }}>
+        <View style={styles.container}>
+          <View style={styles.buttonView}>
+            <Button full transparent onPress={() => this.toggle()}>
+              <Text
+                style={{
+                  paddingTop: 3,
+                  fontWeight: "bold",
+                  color: this.state.tabSelected == 0 ? "#00bcd5" : "#72727d"
+                }}
+              >
+                Recent Chat's
+              </Text>
+            </Button>
+            <View
+              style={[
+                {
+                  height: 3,
+                  backgroundColor:
+                    this.state.tabSelected == 0 ? "#00bcd5" : "white"
+                },
+                styles.shadow
+              ]}
+            />
+          </View>
 
-        <View style={styles.buttonView}>
-          <Button full transparent onPress={() => this.toggle()}>
-            <Text
-              style={{
-                paddingTop: 3,
-                fontWeight: "bold",
-                color: this.state.tabSelected != 0 ? "#00bcd5" : "#72727d"
-              }}
-            >
-              Active
-            </Text>
-          </Button>
-          <View
-            style={[
-              {
-                height: 3,
-                backgroundColor:
-                  this.state.tabSelected != 0 ? "#00bcd5" : "white"
-              },
-              styles.shadow
-            ]}
-          />
+          <View style={styles.buttonView}>
+            <Button full transparent onPress={() => this.toggle()}>
+              <Text
+                style={{
+                  paddingTop: 3,
+                  fontWeight: "bold",
+                  color: this.state.tabSelected == 1 ? "#00bcd5" : "#72727d"
+                }}
+              >
+                Active
+              </Text>
+            </Button>
+            <View
+              style={[
+                {
+                  height: 3,
+                  backgroundColor:
+                    this.state.tabSelected == 1 ? "#00bcd5" : "white"
+                },
+                styles.shadow
+              ]}
+            />
+          </View>
         </View>
+        {this.getSelectedList()}
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     flexDirection: "row",
-    height: 40,
+    height: 43,
     backgroundColor: "white"
   },
   buttonView: {
@@ -83,4 +154,4 @@ const styles = StyleSheet.create({
     shadowColor: "grey",
     shadowOffset: { height: 1, width: 0 }
   }
-});
+})
