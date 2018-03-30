c2467e0dccba5c9e5093c63a535a2beec7f5cd51
CRUSADER1912 <saldanha.linus@gmail.com>
2018-03-30 21:42:11 +0530

    Added filterOut UI component

--- ShaadiLite/src/components/Inbox/FilteredOut.js
+++ ShaadiLite/src/components/Inbox/FilteredOut.js
@@ -1,13 +1,54 @@
 /* @flow */
 
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import FilteredOutCardComponent from "../FilteredOutCardComponent";
+import EmptyView from "../Chat/EmtpyView";
+import axios from "axios";
 
 export default class FilteredOut extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loading: false,
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    this.fetchFilteredOutList();
+  }
+
+  async fetchFilteredOutList() {
+    const url = `https://randomuser.me/api/?page=1&results=20`;
+    this.setState({ loading: true });
+
+    const users = await axios
+      .get(url)
+      .then(res => {
+        this.setState({
+          data: res.data.results,
+          error: res.error || null,
+          loading: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>I'm the FilteredOut component</Text>
+        <FlatList
+          ListEmptyComponent={
+            <EmptyView loadertext="Updating FilteredOut List" />
+          }
+          data={this.state.data}
+          renderItem={({ item }) => <FilteredOutCardComponent profile={item} />}
+          keyExtractor={item => item.email}
+        />
       </View>
     );
   }
