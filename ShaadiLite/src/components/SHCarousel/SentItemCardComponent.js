c8f183c2e9f8ed22c5065fb80aecab589cf34629
CRUSADER1912 <saldanha.linus@gmail.com>
2018-03-31 21:36:58 +0530

    Added body and left native base styles for card component

--- ShaadiLite/src/components/SHCarousel/SentItemCardComponent.js
+++ ShaadiLite/src/components/SHCarousel/SentItemCardComponent.js
@@ -1,39 +1,25 @@
 import React from "react";
 import { View, Image, StyleSheet, Text } from "react-native";
-import { Card, CardItem } from "native-base";
+import { Card, CardItem, Left, Body, Thumbnail } from "native-base";
 
-export default class ChatAvatar extends React.Component {
+export default class SentItemCardComponent extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  getPlaceHolderImage = () => {
-    var icon =
-      this.props.profile.gender == "female"
-        ? require("../../assets/Silhouette_Female.png")
-        : require("../../assets/Silhouette_Male.png");
-    return icon;
-  };
-
-  getChatStatusImage = () => {
-    var icon =
-      this.props.profile.gender == "female"
-        ? require("../../assets/chatStatusIdle.png")
-        : require("../../assets/chatStatusOnline.png");
-    return icon;
-  };
-
   render() {
     return (
       <Card>
-        <CardItem cardBody>
-          <View style={styles.container}>
-            <Image
-              style={styles.image}
-              defaultSource={this.getPlaceHolderImage()}
-              source={{ uri: this.props.profile.picture.thumbnail }}
-            />
-          </View>
+        <CardItem>
+          <Left>
+            <Thumbnail source={{ uri: this.props.profile.picture.thumbnail }} />
+            <Body>
+              <Text>CRUSADER12</Text>
+              <Text note>April 15, 2016</Text>
+              <Text note>April 15, 2016</Text>
+              <Text note>April 15, 2016</Text>
+            </Body>
+          </Left>
         </CardItem>
       </Card>
     );
@@ -44,7 +30,7 @@ const styles = StyleSheet.create({
   container: {
     marginLeft: 10,
     marginTop: 10,
-    flexDirection: "column"
+    backgroundColor: "blue"
   },
   image: {
     flex: 1,
