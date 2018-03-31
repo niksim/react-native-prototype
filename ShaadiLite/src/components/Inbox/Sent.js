2584eacccc2301d786f5c8c459c23b0f07796365
Panchsheela <panchsheela92@gmail.com>
2018-03-31 20:25:24 +0530

    Sent Item From Panch

--- ShaadiLite/src/components/Inbox/Sent.js
+++ ShaadiLite/src/components/Inbox/Sent.js
@@ -1,13 +1,56 @@
 /* @flow */
 
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import SentItemCardComponent from "../SHCarousel/SentItemCardComponent";
+import EmptyView from "../Chat/EmtpyView";
+import axios from "axios";
 
-export default class Sent extends Component {
+export default class SentItem
+ extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loading: false,
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    this.fetchSentItemList();
+  }
+
+  async fetchSentItemList() {
+    const url = `https://randomuser.me/api/?page=1&results=20`;
+    this.setState({ loading: true });
+
+    const users = await axios
+      .get(url)
+      .then(res => {
+        this.setState({
+          data: res.data.results,
+          error: res.error || null,
+          loading: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  }
+
   render() {
     return (
+      
       <View style={styles.container}>
-        <Text>I'm the Sent component</Text>
+        <FlatList
+          ListEmptyComponent={
+            <EmptyView loadertext="Updating Sent Item List" />
+          }
+          data={this.state.data}
+          renderItem={({ item }) => <SentItemCardComponent profile={item} />}
+          keyExtractor={item => item.email}
+        />
       </View>
     );
   }
