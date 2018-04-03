a7edd0d7b26741d53a3b80e0b6ee2dcaffc9372b
Siddhesh Chavan <siddhesh.b.chavan@gmail.com>
2018-04-03 22:58:12 +0530

    - Added new profile images to be displayed under CardItem in Matches tab. - Applied some additional styles in order to have card size in aspect ratio. - Set a gradient with transparency over ImageBackground to place profile details.
    - Added new profile images to be displayed under CardItem in Matches tab.
    - Applied some additional styles in order to have card size in aspect ratio.
    - Set a gradient with transparency over ImageBackground to place profile details.

--- ShaadiLite/src/components/CardComponent.js
+++ ShaadiLite/src/components/CardComponent.js
@@ -1,6 +1,6 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ImageBackground, Image } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Image, Dimensions } from 'react-native';
 
 import { Card, CardItem, Thumbnail, Body, Left, Button, Icon } from 'native-base';
 import { LinearGradient } from 'expo';
@@ -11,18 +11,17 @@ class CardComponent extends Component {
 
         const images = {
             "1": require('../../assets/image_1.jpg'),
-            "2": require('../../assets/ameya.jpg'),
-            "3": require('../../assets/ameya.jpg')
+            "2": require('../../assets/image_2.jpg'),
+            "3": require('../../assets/image_3.jpg')
         }
 
         return (
-            <Card>
-
-                <CardItem>
-                    <ImageBackground source={images[this.props.imageSource]} style={{ width: null, height: 200, flex: 1 ,justifyContent: "flex-end"}}>
-                        <View style={{ marginBottom: 0, width: null, height: 100, flexDirection: "row" }}>
+            <Card style={styles.cardContainer}>
+                <CardItem cardBody>
+                    <ImageBackground source={images[this.props.imageSource]} style={styles.image}>
+                        <View style={{ marginBottom: 0, width: null, height: 300, flexDirection: "row" }}>
                             <LinearGradient
-                                colors={['rgba(2.0,2.0,2.0,1.0)','rgba(2.0,2.0,2.0,1.0)']}
+                                colors={['rgba(1.0,1.0,1.0,0.0)', 'rgba(1.0,1.0,1.0,0.4)', 'rgba(1.0,1.0,1.0,0.7)', 'rgba(1.0,1.0,1.0,0.95)']}
                                 style={{
                                     position: 'absolute',
                                     left: 0,
@@ -30,12 +29,11 @@ class CardComponent extends Component {
                                     bottom: 0,
                                     top: 0,
                                 }}
-                            /></View>
+                            />
+                        </View>
                     </ImageBackground>
-
                 </CardItem>
-
-            </Card>
+            </ Card>
         );
     }
 }
@@ -48,6 +46,25 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#2c3e50',
     },
+    cardContainer: {
+        flex: 1,
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 10,
+        marginRight: 10,
+        // borderRadius: 10.0,
+        // borderColor: "red",
+        // cornerRadius: 50.0,
+        shadowOpacity: 5.0,
+        shadowRadius: 5.0,
+    },
+    image: {
+        flex: 1,
+        justifyContent: "flex-end",
+        width: Dimensions.get('window').width - 20,
+        height: (Dimensions.get('window').width) * 1.3386,
+    },
+
 });
 
 //make this component available to the app
